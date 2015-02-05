6b07ee24305d3a8c9ce5188f891fa18bda69e5d5
Daniel Soucy <soucydi194@potsdam.edu>
2015-02-05 18:02:09 -0500

    did more work on square, gameboard

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -1,3 +1,3 @@
 public class GameBoard {
-    Square [][] squares = new Square[9][9];
+    private Square [][] squares = new Square[9][9];
 }
--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -1,4 +1,12 @@
 public class Square {
+    private int file; // 0-8
+    private int rank; // 0-8
+    private Pawn pawn; 
 
 
+    public Square(int file, int rank) {
+        this.file = file;
+        this.rank = rank;
+        pawn = null;
+    }
 }
