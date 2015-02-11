403697a5db1d85c37c986737a36905632d137159
Daniel Soucy <soucydi194@potsdam.edu>
2015-02-11 18:49:56 -0500

    added classes for fence and point

---
+++ src/main/java/Fence.java
@@ -0,0 +1,8 @@
+public class Fence {
+    public enum orientation {
+        Horizontal, Vertical
+    }
+    private orientation orient;
+    private Point bottom;
+    private Point top;
+}
---
+++ src/main/java/Point.java
@@ -0,0 +1,4 @@
+public class Point {
+    private int x; // rank
+    private int y; // file
+}
