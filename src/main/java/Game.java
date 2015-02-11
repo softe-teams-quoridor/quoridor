46e916c29a88cde998313162db691a8f2255b537
Daniel Soucy <soucydi194@potsdam.edu>
2015-02-11 18:42:46 -0500

    added Game class, tracks the game's state

---
+++ src/main/java/Game.java
@@ -0,0 +1,9 @@
+/* 
+   this class tracks the game's state. 
+*/
+
+public class Game {
+    private int turn; // whose turn is it?
+    private GameBoard board;
+    private int [] walls; // how many walls each player has
+}
--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -1,7 +1,7 @@
 public class Square {
     private int file; // 0-8
     private int rank; // 0-8
-    private Pawn pawn; 
+    private Pawn pawn; // can be null pointer or an occupant
 
 
     public Square(int file, int rank) {
