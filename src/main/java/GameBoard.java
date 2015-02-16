4b618aa42ddbf6b032f091d263b235451a3a4c6f
Collin Walling <wallincl198@potsdam.edu>
2015-02-16 17:06:59 -0500

    Commit GameBoard to see how merging works

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -1,3 +1,18 @@
+/** GameBoard.java - CIS405 - teams
+  * _______________________________________________________
+  * GameBoard object to represent a 9x9 grid for the 
+  * quoridor game
+  *
+  * ----------------------- METHODS -----------------------
+  *
+  *
+  * Considerations
+  *     Saturday (2/14) 
+  *     if this object is only going to represent an array,
+  *     then why not just have a Square array called 
+  *     gameBoard within the Game class? - Walling
+  */
+
 public class GameBoard {
     private Square [][] squares = new Square[9][9];
 }
