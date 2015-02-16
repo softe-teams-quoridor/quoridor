2b80b8da53dc55924c98bfbc310804acb415dcff
Montana Earle <earlems197@potsdam.edu>
2015-02-16 17:09:08 -0500

    added horizontal and vertical wall pointers to fields

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -2,6 +2,8 @@ public class Square {
     private int file; // 0-8
     private int rank; // 0-8
     private Pawn pawn; 
+    private Wall horizontal;
+    private Wall vertical;
 
 
     public Square(int file, int rank) {
