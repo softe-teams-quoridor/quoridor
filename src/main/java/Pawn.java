5cf5be9993957d459205001ab291445bec10835a
Montana Earle <earlems197@potsdam.edu>
2015-02-16 18:04:14 -0500

    added a constructor, parameters: identifier and color (ints)

--- src/main/java/Pawn.java
+++ src/main/java/Pawn.java
@@ -5,4 +5,8 @@ public class Pawn {
     //1 = red, 2 = green, 3 = yellow, 4 = pink
     public int color;
     // and maybe a picture, anybody know how to do this? 
+    public Pawn(int identifier, int color){
+	this.identifier = identifier;
+	this.color = color;
+    }
 } 
