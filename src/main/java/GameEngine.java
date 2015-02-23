45bdd851fc7001447e7e014eabc92675152d4ca8
dansoucy <soucydi194@potsdam.edu>
2015-02-23 16:18:31 -0500

    created crude GameEngine class

---
+++ src/main/java/GameEngine.java
@@ -0,0 +1,23 @@
+/** GameEngine.java - CIS405 - teams
+  * Last Edit: February 23, 2014
+  * _______________________________________________________
+  *
+  * this class is used by both the client and the server!
+  * 
+  * ----------------------- METHODS -----------------------
+  *
+  * validate(GameBoard, String) --> returns true if the string represents
+  *                                 a legal move on that gameboard
+  * 
+  */
+
+public class GameEngine {
+
+    /**
+      * returns true if the string represents a legal move on that gameboard
+      */
+    public boolean validate(GameBoard board, String move) {
+        // FIXME 
+        return true; // NO RULES !!!!!!!!!!!! IT'S ANARCHY !!!!!!!!!!!!!!!!!!!!
+    }
+}
