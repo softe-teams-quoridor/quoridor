695b4219505a5aa0d4a777f5b9f1f54b1f4d256c
dansoucy <soucydi194@potsdam.edu>
2015-02-22 22:34:35 -0500

    added PlayerTest.java

---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author blad
+ *
+ */
+public class PlayerTest {
+    @Before
+    public void empty() throws Exception {
+        // do whatever you want here
+    }
+
+    @Test
+    public void testPlayerConstructor() throws Exception {
+//         Player player = new Player(3, 1);
+//         assertNotNull("Player() returned null", player);
+    }
+}
