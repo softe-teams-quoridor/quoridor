bda7a9312ab1edb182a4b31a49b7dd8b654ab8f8
dansoucy <soucydi194@potsdam.edu>
2015-03-09 15:43:51 -0400

    changed numerals and antinumerals in GameEngine

--- src/main/java/GameEngine.java
+++ src/main/java/GameEngine.java
@@ -12,45 +12,23 @@
   */
 
 public class GameEngine {
+    private static final String [] numerals = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
 
     // converts an int to a string of numerals
     public static String numerals(int x) {
-        switch (x) {
-            case 0: return ""; // empty string
-            case 1: return "I";
-            case 2: return "II";
-            case 3: return "III";
-            case 4: return "IV";
-            case 5: return "V";
-            case 6: return "VI";
-            case 7: return "VII";
-            case 8: return "VIII";
-            case 9: return "IX";
-            case 10: return "X";
-            default: return "@@@@@@@"; // this should never happen
-        }
+        assert (0 < x && x <= 10);
+        return numerals[x];
     }
 
     // does the opposite of numerals
     // FIXME
     public static int antiNumerals(String str) {
-        /*
-        switch (str) {
-            case "": return 0; // empty string
-            case "I": return 1;
-            case "II": return 2;
-            case "III": return 3;
-            case "IV": return 4;
-            case "V": return 5;
-            case "VI": return 6;
-            case "VII": return 7;
-            case "VIII": return 8;
-            case "IX": return 9;
-            case "X": return 10;
-            default: return -1; // this should never happen
+        for (int i = 0; i < 10; i++) {
+            if (str.equals(numerals[i])) {
+                return i;
+            }
         }
-        */
-        return -1; // FIXME
+        return -1; // this should never happen
     }
 
 
