4b316308b768b0fbbd334b2a60aa073f6f8a6bc2
Scott Forbes <forbesst196@potsdam.edu>
2015-03-10 20:08:57 -0400

    Can move player in Demo
    Works now till I can figure out how to use arrows instead.
    In the terminal use:
    8=up, 6=right, 2=down, 4=left.

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -5,32 +5,37 @@ import java.util.*;
 
 
 public class Demo{
+    static Scanner UserInput = new Scanner (System.in);
     public static void main(String[]args)throws InterruptedException{
         GameBoard nBoard=new GameBoard();
         Player tylor=new Player("Tylor",nBoard.getSquare(1,1),10);
         Player nTylor=new Player("Not Tylor", nBoard.getSquare(2,2),10);
         GameboardFrame g1=new GameboardFrame(nBoard);
-        int x=4, y=8, iExit=0;
-        do{
-            //for(x=0; x<9; x++){
-	    nBoard.addPlayer(tylor, x, y);
-	    g1.update(nBoard);
-	    Thread.sleep(50);
-	    nBoard.removePlayer(x,y);
+        int x=4, y=8, iExit=0, i = 0;
+        while (iExit==0){
 	    if(y==0)
 	    {
 		iExit=1;
 	    }
-	    y--;
-	} while (iExit==0);
-                
-                //nBoard.addPlayer(nTylor, x, y);
-                //g1.update(nBoard);
-                //Thread.sleep(50);
-                //nBoard.removePlayer(x,y);
-            //}
-        //}
+	    nBoard.addPlayer(tylor, x, y);
+	    g1.update(nBoard);
+	    Thread.sleep(50);
+	    nBoard.removePlayer(x,y);
+	    i = UserInput.nextInt();
+	    switch (i){
+		case 8: y--;
+		    break;
+		    
+		case 2: y++;
+		    break;
+		    
+		case 4: x--;
+		    break;
+		
+		case 6: x++;
+		    break;
+	    }
+            //for(x=0; x<9; x++){
+	}
     }
-
-
-}
+} //End Demo
