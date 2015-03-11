80fa6a1001ef7c83af379bfe7577ae01df82f3aa
dansoucy <soucydi194@potsdam.edu>
2015-03-11 15:23:46 -0400

    made Game.java able to cycle through servers

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,6 +2,15 @@
    this class tracks the game's state. 
 */
 
+import java.util.*;
+
+// networking stuff... 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Game {
 
 
@@ -11,11 +20,68 @@ public class Game {
     private GameBoard board;  // board used to keep track of game progress
     private Player[] players; // array of players
 
-
+    /*
+     * prints a friendly message and exits
+     * @param an int to return to the OS
+     */
+    private static void usage(int error) {
+        System.err.println("usage: java Game <port> <port>");
+        System.exit(error);
+    }
 
     public static void main ( String[] args ) {
+        if (args.length < 2) {
+            usage(1);
+        }
+
+        int [] ports = new int[args.length];
+        for (int i = 0; i < args.length; i++) { 
+            try {
+                ports[i] = Integer.parseInt(args[i]);
+            } catch (Exception e) {
+                usage(2);
+            }
+        }
+        System.out.println(Arrays.toString(ports));
+
 
         // Connect to players
+        PrintStream [] outStreams = new PrintStream [ports.length];
+        Scanner     [] inStreams  = new Scanner     [ports.length];
+//         for (int port : ports) {
+        for (int i = 0; i < ports.length; i++) {
+            try {
+                Socket socket = new Socket("localhost", ports[i]);
+                PrintStream sout = new PrintStream(socket.getOutputStream());
+                outStreams[i] = sout;
+                Scanner sin = new Scanner(socket.getInputStream());
+                inStreams[i] = sin;
+            } catch (UnknownHostException uhe) {
+                // the host name provided could not be resolved
+                uhe.printStackTrace();
+                System.exit(1);
+            } catch (IOException ioe) {
+                // there was a standard input/output error (lower-level)
+                ioe.printStackTrace();
+                System.exit(1);
+            }
+        }
+        for (int i = 0; i < ports.length; i++) {
+            if (outStreams[i] == null) {
+                System.out.println("outStreams[" + i + "] is null!");
+            }
+            if (inStreams[i] == null) {
+                System.out.println("inStreams[" + i + "] is null!");
+            }
+        }
+        for (int i = 0; i < ports.length; i++) {
+            outStreams[i].println("make your move");
+            String response = inStreams[i].nextLine();
+            System.out.println("received: " + response);
+        }
+        System.out.println("done!");
+
+
         // - this will give us the number of players playing (2 or 4)
 
         // Instantiate Players array
