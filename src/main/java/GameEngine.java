258cf8f1f47d7c531970b87ff7925af74d662d13
Collin Walling <wallincl198@potsdam.edu>
2015-03-13 00:01:27 -0400

    Added documentation to GameEngine.java

--- src/main/java/GameEngine.java
+++ src/main/java/GameEngine.java
@@ -5,13 +5,15 @@
   * 
   * ----------------------- METHODS -----------------------
   *
-  * validate(GameBoard, String) --> returns true if the string represents
-  *                                 a legal move on that gameboard
-  * toNumerals(int), 
-  * fromNumerals(String),
-  * toLetters(int),
-  * fromLetters(char) --> conversions between ints and numerals/letters
-  * 
+  * validate(GameBoard, String)  --> returns true if the string represents
+  *                                    a legal move on that gameboard
+  * toNumerals(int)              --> converts int to a numeral 
+  * fromNumerals(String)         --> converts string to an int
+  * toLetters(int)               --> converts int to a letter ex 0 -> A
+  * fromLetters(char)            --> conversions between ints and numerals/letters
+  * getSquare(GameBoard, string) --> constructs a square based on the move string
+  * checkVictory(GameBoard,      --> checks if a player has won the game 
+  *              Players[])
   */
 
 public class GameEngine {
@@ -21,7 +23,10 @@ public class GameEngine {
     private static final char [] letters = {'A', 'B', 'C', 'D',
                                             'E', 'F', 'G', 'H', 'I', 'Z'};
 
-    // converts an int to a string of roman numerals
+    /** 
+      * converts an int to a string of roman numerals
+      * @param x: integer to convert to a numeral
+      */
     public static String toNumerals(int x) {
         if (x < 0 || letters.length < x) {
             return "@@@@@@@@@@@@@@"; // this should never happen
@@ -29,7 +34,10 @@ public class GameEngine {
         return numerals[x];
     }
 
-    // does the opposite of toNumerals
+    /**
+      * converts a roman numeral to an integer
+      * @param str: string to convert
+      */
     public static int fromNumerals(String str) {
         for (int i = 0; i < numerals.length; i++) {
             if (str.equals(numerals[i])) {
@@ -39,7 +47,10 @@ public class GameEngine {
         return -1; // this should never happen
     }
 
-    // converts an int to a char A-I
+    /**
+      * converts an int to a char A-I
+      * @param x: integer to convert to a numeral
+      */
     public static char toLetters(int x) {
         if (x < 0 || letters.length < x) {
             return 'Z'; // this should never happen
@@ -47,7 +58,10 @@ public class GameEngine {
         return letters[x];
     }
 
-    // does the opposite of toLetters
+    /**
+      * converts a character to an integer value
+      * @param ch: character to convert
+      */
     public static int fromLetters(char ch) {
         for (int i = 0; i < numerals.length; i++) {
             if (letters[i] == ch) {
@@ -57,10 +71,11 @@ public class GameEngine {
         return -1; // this should never happen
     }
 
-
-
     /**
       * returns true if the string represents a legal move on that gameboard
+      * @param board: GameBoard object to move on
+      * @param p: Player object requesting the move
+      * @param move: String that contains the move destination
       */
     public static boolean validate(GameBoard board, Player p, String move) {
         //***TEST ME****
@@ -89,7 +104,7 @@ public class GameEngine {
         return false; 
     }
 
-    /* 
+    /** 
      *  returns true if the string represents a possibly legal move
      *  i.e. the string is of the correct format
      *   FIXME: this currently breaks if you input a wall-placing move
@@ -124,8 +139,9 @@ public class GameEngine {
         return false;
     }
 
-    /* this returns a square on the board
-     *   FIXME: this currently breaks if you input a wall-placing move
+    /**
+     * this returns a square on the board
+     * FIXME: this currently breaks if you input a wall-placing move
      * @param board
      * @param move: a string representing a legal move
      */
@@ -148,6 +164,11 @@ public class GameEngine {
         }
     }
 
+    /**
+      * Checks if a player has won the game
+      * @param board: GameBoard to check
+      * @param players: array of players to check if they have won
+      */
     public static boolean checkVictory(GameBoard board, Player[] players) {
 
         if(players.length == 2) {
