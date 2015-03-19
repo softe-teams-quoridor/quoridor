7e6d49b0857c7f4c253f52bf0206346900a9885c
Eric Cavanagh <cavanaed198@potsdam.edu>
2015-03-18 22:36:23 -0400

    - Finished the GameBoardTest added and fixed all test - Removed old code in Player.java that used tob get location - Removed method in GameBoard that got the player location     from the player number

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -120,7 +120,7 @@ public class GameBoard {
      * @param y the row of the gameboard
      * @return true if location is on board, false otherwise
      */
-    private boolean validLoc(int x, int y) {
+    protected boolean validLoc(int x, int y) {
         return (x >= 0 && x < COLUMNS && y >= 0 && y < ROWS);
     }
 
@@ -156,9 +156,6 @@ public class GameBoard {
             int y = rowInd & 15;
 
             assert (validLoc(x, y));
-            assert (x >= 0 && x < 9);
-            assert (y >= 0 && y < 9);
-            assert (i >= 0 && i < players.length);
             this.addPlayer(players[i], x, y);
             assert (i >= 0 && i < this.playerLocs.length);
             this.playerLocs[i] = getSquare(x, y);
@@ -167,11 +164,10 @@ public class GameBoard {
         }
     }
 
+    //*************************************************************************
+
     public Square getPlayerLoc(Player player) {
         return playerLocs[player.getPlayerNo()];
     }
-
-    public Square getPlayerLoc(int playerNo) {
-        return playerLocs[playerNo];
-    }
+    
 }
