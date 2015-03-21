c0bd691c91ba9a47d5a6e4c50ab5ec1cbc075be2
Collin <collin.walling@gmail.com>
2015-03-21 13:47:55 -0400

    Cleaned up Wall.java     - Enum has been fixed up it now has START and END instead of        BOTTOM and RIGHT     - Added new constructor to take the enum     - Added new method to return the enum     **** Consider if Wall.java can be merged with Square.java

--- src/main/java/Wall.java
+++ src/main/java/Wall.java
@@ -1,46 +1,70 @@
 /* Wall.java - CIS405 - teams
- * Last Edit: March 20, 2015
- * _______________________________________________________
+ * Last Edit: March 21, 2015
+ * ____________________________________________________________________________
  *
- * a wall that is placed on a gameboard square
- * 
- * Note: this indicates part of a wall; two squares should
- *    be given a wall object upon a valid wall placement
- *    one wall on a square should be set to true and the
- *    other wall on an adjacent square should be set to
- *    true in order to maintain the 1x2 size of a Quoridor
- *    wall
+ * consider merging this object with Square.java
  *
- * ----------------------- METHODS -----------------------
+ * a wall that is placed on a gameboard square. a square with a wall has either
+ *  of three values: null (no wall present), START (this is the beginning-half
+ *  of a wall piece, or END (this is the ending-half of a wall piece)
  *
- * Wall(bool)   --> constructor
- * boolean getIsStart() --> returns if this wall is a start or end piece
+ * for valid wall placement, a two squares should be given a new wall, one 
+ *  being passed START, and one being passed END
+ *
+ * --------------------------------- METHODS ----------------------------------
+ *
+ * Wall(bool)                 --> constructor using boolean
+ * Wall(Orientation)          --> constructor using Orientation enum
+ * boolean getIsStart()       --> returns if this wall is a start or end piece
+ * Orientation getOrientation --> returns the orientation direction of the wall
  */
 
 public class Wall{
 
-    public enum orientation {
-        Bottom, Right
+    public enum Orientation {
+        START, END 
     }
 
-    // Data Members
-    private boolean isStart; // Indicates if this wall is a beginning
-    // or end piece    
+    private boolean isStart; // is this wall is a beginning or end piece?
+    private Orientation direction; 
 
-    /** Constructor
+    //*************************************************************************
+    
+    /**
      * instantiates a Wall object
-     * @param start = indicates if this wall begins at the 
-     *                top or left (true), or bottom or right (false)
+     * @param start indicates if the wall piece is a start or end piece 
      */                 
     public Wall(boolean start){
         isStart = start;
     }
 
+    //*************************************************************************
+
+    /**
+     * instantiates a Wall object
+     * @param direction indicates if the wall piece is a start or end piece 
+     */                 
+    public Wall ( Orientation direction ) {
+        this.direction = direction;
+    }
+
+    //*************************************************************************
+
     /**
      * returns whether this wall is the start or end point
-     * @return returns boolean
+     * @return true if wall piece is the start-half, false if end-half
      */
     public boolean getIsStart() {
         return isStart;
+    }
+
+    //*************************************************************************
+
+    /**
+     * returns whether this wall is the start or end point
+     * @return the Orientation direction enum
+     */
+    public Orientation getOrientation ( ) {
+        return direction;
     }
 }
