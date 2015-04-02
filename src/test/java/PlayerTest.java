39fd51b1896cc17a635bf0df874a4adc5effad7e
Eric Cavanagh <cavanaed198@potsdam.edu>
2015-04-02 18:28:53 -0400

    Updated PlayerTest to now use a Queue

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -2,49 +2,56 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.Before;
+import java.util.Queue;
+import java.util.LinkedList;
 
 /**
- * @author ECav
+ * teams
  * Rebuilt on 3/19/15
  */
 public class PlayerTest {
 
     private static final int NUM_PLAYERS = 4;
-    private Player [] players;
+//    private Player [] players;
+    private Queue<Player> players;
     private int walls;
 
     @Before
     public void beef() throws Exception {
-        // Setup a players array of NUM_PLAYERS players
-        //Player.resetPlayerNos();
-        players = new Player[NUM_PLAYERS];
+        // Setup a players queue of NUM_PLAYERS players
+        players = new LinkedList<Player>();
         walls = 20 / NUM_PLAYERS;
        for(int i = 0; i < NUM_PLAYERS; i++)
-            players[i] = new Player(i, walls); 
+            players.add(new Player(i, walls)); 
         
     }
 
     @Test
     public void getPlayerNoTest() throws Exception {
         // Check all player numbers in array
-        for(int i = 0; i < NUM_PLAYERS; i++) 
-            assertEquals(players[i].getPlayerNo(), i);
-         
+        for(int i = 0; i < NUM_PLAYERS; i++) {
+            assertEquals(players.peek().getPlayerNo(), i);
+            players.add(players.remove());
+        }         
     }
 
     @Test
     public void getNameTest() throws Exception {
         // Check all names of players 
-        for(int i = 0; i < NUM_PLAYERS; i++)
-            assertEquals(players[i].getName(), "player_" + i);
+        for(int i = 0; i < NUM_PLAYERS; i++) {
+            assertEquals(players.peek().getName(), "player_" + i);
+            players.add(players.remove());
+        }
     
     }
 
     @Test
     public void getNumWallsTest() throws Exception {
         // Make sure all players have correct amount of walls
-        for(int i = 0; i < NUM_PLAYERS; i++) 
-            assertEquals(players[i].getNumWalls(), walls); 
+        for(int i = 0; i < NUM_PLAYERS; i++) { 
+            assertEquals(players.peek().getNumWalls(), walls);
+            players.add(players.remove());
+        }
     }
 
     @Test
@@ -52,12 +59,16 @@ public class PlayerTest {
         
         // Place all walls for all players
         for(int i = 0; i < NUM_PLAYERS; i++)
-            for(int j = 0; j < walls; j++)
-                assertTrue(players[i].useWall());
+            for(int j = 0; j < walls; j++) {
+                assertTrue(players.peek().useWall());
+                players.add(players.remove());
+            }
 
         // check to make sure all players cannot place walls
-        for(int i = 0; i < NUM_PLAYERS; i++)
-            assertFalse(players[i].useWall());
+        for(int i = 0; i < NUM_PLAYERS; i++) {
+            assertFalse(players.peek().useWall());
+            players.add(players.remove());
+        }
     
     }
 
