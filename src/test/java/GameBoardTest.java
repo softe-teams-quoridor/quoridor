7e070237bf3c77e905ec3033e378e788acd0eadd
Collin Walling <wallincl198@potsdam.edu>
2015-04-02 20:15:46 -0400

    More tests added to GameBoardTest

--- src/test/java/GameBoardTest.java
+++ src/test/java/GameBoardTest.java
@@ -31,22 +31,7 @@ public class GameBoardTest {
         board = new GameBoard(players);
         assertNotNull("board should not be null!", board);
     } 
-/*
-   // DO NOT TEST PRIVATE ADDPLAYER
-    @Test
-    public void testAddPlayer() {
-        board.addPlayer(, 1, 1);
-        assertEquals(board.getPlayer(1,1), players[0]);
-    }
-    // DO NOT TEST PRIVATE VALIDLOC
-    @Test
-    public void testValidLoc() throws Exception {
-        for(int i = 0; i < 9; i++)
-            for(int j = 0; j < 9; j++)
-                assertTrue(board.validLoc(i,j));
-        assertFalse(board.validLoc(10,12));
-    }
-*/
+    
     /* Ensures all squares have been properly assigned coordinates */
     @Test
     public void testGetSquares() throws Exception {
@@ -56,7 +41,6 @@ public class GameBoardTest {
                 assertEquals(board.getSquare(i,j).getY(), j);
             }
     }
-
     
     /* Ensures we can retrieve a player's start location based on coordinates*/
     @Test
@@ -73,35 +57,55 @@ public class GameBoardTest {
         players.add(players.remove());
         assertEquals(board.getPlayer(8,4),players.peek());
     }
-
     
     /* Ensures we can retrieve a player's start location based on player num */
     @Test
     public void testGetPlayerLocWithPNOFromStartLocs() throws Exception {
-        // Test p0
+        // test p0
         assertEquals(board.getPlayer(players.peek().getPlayerNo()),players.peek());
-        // Shuffle; test p1
-        players.add(players.remove());
+        // test p1
+        players.remove();
         assertEquals(board.getPlayer(players.peek().getPlayerNo()),players.peek());
-        // Shuffle; test p2
-        players.add(players.remove());
+        // test p2
+        players.remove();
         assertEquals(board.getPlayer(players.peek().getPlayerNo()),players.peek());
-        // Shuffle; test p3
-        players.add(players.remove());
+        // test p3
+        players.remove();
         assertEquals(board.getPlayer(players.peek().getPlayerNo()),players.peek());
+    }
+
+    /* Ensure we can remove players from the board */
+    @Test
+    public void testRemovePlayer() throws Exception {
+        // test p0 was removed
+        board.removePlayer(players.peek());
+        assertNull(board.getPlayer(4,0));
+        // test p1 was removed
+        players.remove();
+        board.removePlayer(players.peek());
+        assertNull(board.getPlayer(4,8));
+        // test p2 was removed
+        players.remove();
+        board.removePlayer(players.peek());
+        assertNull(board.getPlayer(0,4));
+        // test p3 was removed
+        players.remove();
+        board.removePlayer(players.peek());
+        assertNull(board.getPlayer(8,4));
     }
 
     /* Ensures that getPlayer can return null */
     @Test
     public void testGetPlayerReturnsNull() throws Exception {
-        // Remove all players from the queue
+        // remove all players from the board
         for(int i = 0; i < NUM_PLAYERS; i++) {
             board.removePlayer(players.peek());
             players.remove();
-        } 
+        }
+        // assert all squares do not have a player
         for ( int i = 0; i < GameBoard.COLUMNS; i++ )
             for ( int j = 0; j < GameBoard.ROWS; j++ ) 
-                assertEquals(board.getPlayer(i,j), null);
+                assertNull(board.getPlayer(i,j));
     }
     /*
     
@@ -118,11 +122,6 @@ public class GameBoardTest {
     }
 
     
-    @Test
-    public void testRemovePlayer() {
-        board.removePlayer(players[0]);
-        assertNull(board.getPlayer(4,0));
-    }
     
 
     @Test
