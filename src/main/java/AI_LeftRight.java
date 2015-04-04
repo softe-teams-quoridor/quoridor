050128b00f822fb17bb2a6583115655a139c00d8
dansoucy <soucydi194@potsdam.edu>
2015-04-04 13:54:43 -0400

    AI_LeftRight now works for any player

--- src/main/java/AI_LeftRight.java
+++ src/main/java/AI_LeftRight.java
@@ -1,19 +1,41 @@
+/* this QuoridorAI alternates between stepping left and right */
 public class AI_LeftRight implements QuoridorAI {
-    private boolean direction; 
+    private boolean goLeft; 
     public AI_LeftRight() {
-        direction = false;
+        goLeft = false; // true means step left, false means step right
     }
 
-    public String getMove(GameBoard _, Player p) {
-        direction = (! direction);
-        if (direction) {
-            return "IV-A";
+    public String getMove(GameBoard _, Player player) {
+        goLeft = (! goLeft);
+        if (player.getPlayerNo() == 0) {
+            if (goLeft) {
+                return "IV-A";
+            } else {
+                return "V-A";
+            }
+        } else if (player.getPlayerNo() == 1) {
+            if (goLeft) {
+                return "IV-I";
+            } else {
+                return "V-I";
+            }
+        } else if (player.getPlayerNo() == 2) {
+            if (goLeft) {
+                return "II-E"; // can't go left, that's a border
+            } else {
+                return "I-E";
+            }
         } else {
-            return "V-A";
+            assert (player.getPlayerNo() == 3);
+            if (goLeft) {
+                return "VIII-E";
+            } else {
+                return "IX-E";
+            }
         }
     }
 
     public void reset() {
-        direction = false;
+        goLeft = false;
     }
 }
