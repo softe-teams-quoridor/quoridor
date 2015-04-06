374372373d0b198a8957ac3a4d4f5765d6baebb3
dansoucy <soucydi194@potsdam.edu>
2015-04-06 00:12:15 -0400

    added failing test for Game.countValidNames

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -16,8 +16,27 @@ public class GameTest {
         assertTrue(Game.isValidName("hello"));
         assertTrue(Game.isValidName("Stake-1"));
         assertTrue(Game.isValidName("SnakeA0DF97"));
+        assertFalse(Game.isValidName(null));
         assertFalse(Game.isValidName("Sn akeA0DF97"));
         assertFalse(Game.isValidName("SnhtunsahtenhakeA0DF97"));
         assertFalse(Game.isValidName("S ht nsa ten "));
+    }
+
+    @Test
+    public void testCountValidNames() {
+        String [] sampleNames = {null};
+        assertEquals(0, Game.countValidNames(sampleNames));
+
+        String [] sampleNames2 = {"hello", null};
+        assertEquals(1, Game.countValidNames(sampleNames2));
+
+        String [] sampleNames3 = {"hello", "hutnesa"};
+        assertEquals(2, Game.countValidNames(sampleNames3));
+
+        String [] sampleNames4 = {"hello", "hutnesa", null, "what"};
+        assertEquals(3, Game.countValidNames(sampleNames4));
+
+        String [] sampleNames5 = {"hello", "hutnesa", "null", "what"};
+        assertEquals(4, Game.countValidNames(sampleNames5));
     }
 }
