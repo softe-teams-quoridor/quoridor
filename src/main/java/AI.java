bc33153ca1f6d2be9e93a539bf6be98edce08d4d
Montana Earle <earlems197@potsdam.edu>
2015-04-08 16:36:18 -0400

    implements interface but does nothing

---
+++ src/main/java/AI.java
@@ -0,0 +1,25 @@
+/* This is the actual AI */
+
+public class AI implements QuoridorAI{
+
+    /* constructor:
+	-no parameters */
+    public AI(){
+	
+    }
+    
+    /* get a move string */
+    public String getMove(GameBoard b, Player p){
+	return "FIXME";
+    }
+
+    /* reset an ai to its initial state; used for starting a new game */
+    public void reset(){}
+
+    /* used in names 
+     * don't make this return anything longer than 10 characters though LOOOOL
+     */
+    public String toString(){
+	return "teams";
+    }
+}
