24c75af50b87a60ede8776220140bdedddd4639d
Montana Earle <earlems197@potsdam.edu>
2015-04-08 17:15:24 -0400

    heads in the right direction. ignores walls

--- src/main/java/AI.java
+++ src/main/java/AI.java
@@ -4,15 +4,45 @@ public class AI implements QuoridorAI{
 
     /* constructor:
 	-no parameters */
-    public AI(){
-	
-    }
+    public AI(){}
     
     /* get a move string */
     public String getMove(GameBoard b, Player p){
-	return "FIXME";
+	
+	//get our player's current location
+	Square current = b.getPlayerLoc(p);
+	int x = current.getX();
+	int y = current.getY();
+	
+	//get recheable squares from our location
+	Square [] reachable = GameEngine.reachableAdjacentSquares(b, current);
+	int playerno = p.getPlayerNo();
+
+	Square newLoc = choose(playerno);
+	
+	if(b.isOccupied(x,y))
+	    newLoc = null;
+	
+	for(int i = 0; i < reachable.length; i++)
+	    if(reachable[i].equals(newLoc))
+		 return newLoc.toString();
+	
+	return reachable[0].toString();
     }
 
+    private Square choose(int playerno){
+	if(playerno == 0)
+	    y++;
+	else if(playerno == 1)
+	    y--;
+	else if(playerno == 2)
+	    x++;
+	else
+	    x--;
+	
+	return new Square(x,y);
+    }
+    
     /* reset an ai to its initial state; used for starting a new game */
     public void reset(){}
 
