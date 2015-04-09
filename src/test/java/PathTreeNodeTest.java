d1989153cee361c3ac58faac90cfef5fcdb00a23
Montana Earle <earlems197@potsdam.edu>
2015-04-09 13:50:36 -0400

    Tests PathTreeNode constructor and set methods

---
+++ src/test/java/PathTreeNodeTest.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.util.Queue;
+import java.util.LinkedList;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+public class PathTreeNodeTest{
+    
+    PathTreeNode node;
+    Square sq;
+    
+    /* Instantiates the PathTreeNode for all tests */
+    @Before
+    public void instantiatePathTreeNode() throws Exception {
+        sq = new Square(4,0);
+        node = new PathTreeNode(sq);
+        assertNotNull("node should not be null!", node);
+    }
+    
+    /*assert that constructor builds correctly. All nodes should be null*/
+    @Test
+    public void testConstructor() throws Exception{
+	assertNull("up should be initialized as null", node.getUp());
+	assertNull("down should be initialized as null", node.getDown());
+	assertNull("left should be initialized as null", node.getLeft());
+	assertNull("right should be initialized as null", node.getRight());
+    }
+    
+    /* check that setUp adds a node */
+    @Test 
+    public void testSetUp() throws Exception{
+	node.setUp(new PathTreeNode(new Square(1,1)));
+	assertNotNull("up should not return null after setUp", node.getUp());
+    }
+    
+    /* check that setDown adds a node */
+    @Test 
+    public void testSetDown() throws Exception{
+	node.setDown(new PathTreeNode(new Square(1,1)));
+	assertNotNull("down should not return null after setDown", node.getDown());
+    }
+    
+    /* check that setRight adds a node */
+    @Test 
+    public void testSetRight() throws Exception{
+	node.setRight(new PathTreeNode(new Square(1,1)));
+	assertNotNull("right should not return null after setRight", node.getRight());
+    }
+    
+    /* check that setLeft adds a node */
+    @Test 
+    public void testSetLeft() throws Exception{
+	node.setLeft(new PathTreeNode(new Square(1,1)));
+	assertNotNull("left should not return null after setLeft", node.getLeft());
+    }
+}
