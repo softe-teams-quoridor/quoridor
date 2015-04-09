f550000e67b0f524e9cb374e9e956e109167ccac
Collin <collin.walling@gmail.com>
2015-04-09 02:45:22 -0400

    AI_Ripley: PrintVirtualBoardDemo.java

---
+++ src/main/java/PrintVirtualBoardDemo.java
@@ -0,0 +1,10 @@
+public class PrintVirtualBoardDemo {
+
+    public static void main ( String[] args ) {
+
+        AI_Ripley rip = new AI_Ripley();
+        System.out.println(rip.toString());
+        rip.printVirtualBoard();
+    }
+
+}
