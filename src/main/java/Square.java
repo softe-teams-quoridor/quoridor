4e9307bdcb202466fe12688aaf405299e6919871
Collin <collin.walling@gmail.com>
2015-04-10 01:38:16 -0400

    Square.java: nevermind, I'm stupid. Those methods already exist. Sorry!

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -153,14 +153,6 @@ public class Square {
         return (bottomWall != null);
     }
 
-    public Wall getWallBottom() {
-        return (hasWallBottom) ? bottomWall : null;
-    }
-
-    public Wall getWallRight() {
-        return (hasWallRight) ? rightWall : null;
-    }
-
     //*************************************************************************
 
     /**
