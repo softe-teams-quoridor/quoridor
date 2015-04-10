0eb2308b1114ac88097396e1afb70087ed0c4bc7
Montana Earle <earlems197@potsdam.edu>
2015-04-10 16:41:39 -0400

    reworked the node system

--- src/main/java/PathTreeNode.java
+++ src/main/java/PathTreeNode.java
@@ -1,58 +1,15 @@
 public class PathTreeNode{
     
-    private PathTreeNode up;
-    private PathTreeNode down;
-    private PathTreeNode left;
-    private PathTreeNode right;
-    private Square location;
-    private int i;
+    public PathTreeNode nextAdjFromParent;
+    public PathTreeNode firstAdjFromHere;
+    public Square location;
+    public int i;
     
     public PathTreeNode(Square s, int i){
-	up = null;
-	down = null;
-	left = null;
-	right = null;
+	nextAdjFromParent = null;
+	firstAdjFromHere = null;
 	location = s;
 	this.i = i;
     }
     
-    public int getNum(){
-	return i;
-    }
-    
-    public PathTreeNode getUp(){
-	return up;
-    }
-    
-    public PathTreeNode getDown(){
-	return down;
-    }
-    
-    public PathTreeNode getLeft(){
-	return left;
-    }
-    
-    public PathTreeNode getRight(){
-	return right;
-    }
-    
-    public Square getLocation(){
-	return location;
-    }
-    
-    public void setUp(PathTreeNode p){
-	up = p;
-    }
-    
-    public void setDown(PathTreeNode p){
-	down = p;
-    }
-    
-    public void setRight(PathTreeNode p){
-	right = p;
-    }
-    
-    public void setLeft(PathTreeNode p){
-	left = p;
-    }
 }
