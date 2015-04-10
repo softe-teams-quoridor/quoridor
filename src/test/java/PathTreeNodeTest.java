c7c4ebe5a5ab4ae7b3d713abb31598dc8b52ebbe
Montana Earle <earlems197@potsdam.edu>
2015-04-09 21:40:59 -0400

    fixed test for the added field

--- src/test/java/PathTreeNodeTest.java
+++ src/test/java/PathTreeNodeTest.java
@@ -15,7 +15,7 @@ public class PathTreeNodeTest{
     @Before
     public void instantiatePathTreeNode() throws Exception {
         sq = new Square(4,0);
-        node = new PathTreeNode(sq);
+        node = new PathTreeNode(sq,1);
         assertNotNull("node should not be null!", node);
     }
     
@@ -27,33 +27,34 @@ public class PathTreeNodeTest{
 	assertNull("left should be initialized as null", node.getLeft());
 	assertNull("right should be initialized as null", node.getRight());
 	assertNotNull("location should never be null!", node.getLocation());
+	assertNotNull(node.getNum());
     }
     
     /* check that setUp adds a node */
     @Test 
     public void testSetUp() throws Exception{
-	node.setUp(new PathTreeNode(new Square(1,1)));
+	node.setUp(new PathTreeNode(new Square(1,1), 0));
 	assertNotNull("up should not return null after setUp", node.getUp());
     }
     
     /* check that setDown adds a node */
     @Test 
     public void testSetDown() throws Exception{
-	node.setDown(new PathTreeNode(new Square(1,1)));
+	node.setDown(new PathTreeNode(new Square(1,1), 0));
 	assertNotNull("down should not return null after setDown", node.getDown());
     }
     
     /* check that setRight adds a node */
     @Test 
     public void testSetRight() throws Exception{
-	node.setRight(new PathTreeNode(new Square(1,1)));
+	node.setRight(new PathTreeNode(new Square(1,1), 0));
 	assertNotNull("right should not return null after setRight", node.getRight());
     }
     
     /* check that setLeft adds a node */
     @Test 
     public void testSetLeft() throws Exception{
-	node.setLeft(new PathTreeNode(new Square(1,1)));
+	node.setLeft(new PathTreeNode(new Square(1,1), 0));
 	assertNotNull("left should not return null after setLeft", node.getLeft());
     }
 }
