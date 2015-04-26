10749593ddd83f411ce91357bc683b0baab4ea5e
Montana Earle <earlems197@potsdam.edu>
2015-04-26 18:59:21 -0400

    reworking to work with walls

--- src/main/java/ai/AI.java
+++ src/main/java/ai/AI.java
@@ -14,13 +14,29 @@ public class AI implements QuoridorAI{
 	int x = current.getX();
 	int y = current.getY();
 	
+	//get number of walls we have
+	int numWalls = p.getNumWalls();
+	
+	//if the player doesnt have a wall, there is no use checking for 
+	// a need to use one. move on
+	if(numWalls > 0){
+	    //check if a wall needs to be used
+	    String useWall = checkWalls();
+	    
+	    //if null, move on
+	    if(useWall != null)
+		return useWall;
+	}
+	
 	//get recheable squares from our location
 	Square[] reacheable = GameEngine.reachableAdjacentSquares(b, current);
 	Square[] secondChoice = new Square[reacheable.length];
 	int index = 0;
 	
 	int playerno = p.getPlayerNo();
-		
+	
+	//based on player no, set any moves that are going backwards aside
+	//as secondchioce. Also, set x and y for ideal move
 	if(playerno == 0){
 	    for(int i = 0; i < reacheable.length; i++)
 		if(reacheable[i].getY() < y){
@@ -55,18 +71,23 @@ public class AI implements QuoridorAI{
 	    x--;
 	}
 	
+	//create a square object based on ideal move
 	Square newLoc = new Square(x,y);
 	
+	//if we have the ideal move, return that
 	for(int i = 0; i < reacheable.length; i++){
 	    if(reacheable[i] != null && reacheable[i].equals(newLoc))
 		 return newLoc.toString();
 	}
 	
+	//otherwise return a first choice move
 	for(int i = 0; i < reacheable.length; i++){
 	    if(reacheable[i] != null)
 		 return reacheable[i].toString();
 	}
 	
+	//if both of these are busts, we have to have a second choice move.
+	//All players have at least one move. So return that.
 	return secondChoice[0].toString();
     }
 
@@ -78,5 +99,11 @@ public class AI implements QuoridorAI{
      */
     public String toString(){
 	return "teams";
+    }
+    
+    /* check for a need to use a wall. If there is a need, 
+        return a string that says to use one. Otherwise return null */
+    public String checkWalls(){
+	return null;
     }
 }
