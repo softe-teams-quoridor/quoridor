0f5632baa64c3d5a08ca52ad04f2a0579454c64c
Collin Walling <wallincl198@potsdam.edu>
2015-04-30 21:18:29 -0400

    GraphTest: updated to reflect Graph method changes

--- src/test/java/GraphTest.java
+++ src/test/java/GraphTest.java
@@ -11,40 +11,44 @@ public class GraphTest {
     GameBoard b;
     Graph g;
 
+    /* instantiate necessary objects */
     @Before
     public void buildGraph() throws Exception {
         p = new LinkedList<Player>();
-        p.add(new Player(0,5));
-        p.add(new Player(1,5));
+        p.add(new Player(1,5)); p.add(new Player(0,5));
         b = new GameBoard(p);
         g = new Graph(GameBoard.ROWS * GameBoard.COLUMNS);
     }
 
+    /* test conversion of Square coordinates to a Vertex coordinate */
     @Test
-    public void testGraphAndBoardCoordinateCorrespondance() throws Exception {
+    public void testSquareToVertex() throws Exception {
+        g.buildPath(b, p.peek());
         int count = 0;
         for ( int y = 0; y < GameBoard.ROWS; y++ )
             for ( int x = 0; x < GameBoard.COLUMNS; x++ ) {
-                assertEquals(count, g.linearXY(b.getSquare(x,y)));
+                Vertex v = g.squareToVertex(b.getSquare(x,y));
+                //System.out.println(count + " == " + v.graphLoc + "?");
+                assertEquals(count, v.graphLoc);
                 count++;
             }
     }
 
+    /* test conversion of a Vertex coordinate Squares coordinates */
     @Test
-    public void testTwoDimConversion() throws Exception {
+    public void testVertexToSquare() throws Exception {
         int count = 0;
         for ( int y = 0; y < GameBoard.ROWS; y++ )
             for ( int x = 0; x < GameBoard.COLUMNS; x++ ) {
                 Vertex v = new Vertex(count, -1);
                 Square boardSquare = b.getSquare(x,y);
-                Square graphSquare = g.twoDimXY(v);
-                System.out.println(boardSquare.getX() + " == " + graphSquare.getX() + "?");
-                System.out.println(boardSquare.getY() + " == " + graphSquare.getY() + "?");
+                Square graphSquare = g.vertexToSquare(v);
+                //System.out.println(boardSquare.getX() + " == " + graphSquare.getX() + "?");
+                //System.out.println(boardSquare.getY() + " == " + graphSquare.getY() + "?");
                 assertEquals(boardSquare.getX(),graphSquare.getX());
                 assertEquals(boardSquare.getY(),graphSquare.getY());
                 count++;
             }
-
     }
 
 }
