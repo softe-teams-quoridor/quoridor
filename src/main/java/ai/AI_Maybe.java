fdd9ea4c337a3efe159a4c9ed5003c626946d805
Eric Cavanagh <cavanaed198@potsdam.edu>
2015-05-06 17:02:39 -0400

    Maybe now places walls somewhat logically

--- src/main/java/ai/AI_Maybe.java
+++ src/main/java/ai/AI_Maybe.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class AI_Maybe implements QuoridorAI {
 
     /**
@@ -5,6 +6,7 @@ public class AI_Maybe implements QuoridorAI {
       */
     public AI_Maybe() { }
     
+    //DOCUMENTME
     /**
       * Returns a move.
       *     @param b GameBoard to play on
@@ -13,62 +15,118 @@ public class AI_Maybe implements QuoridorAI {
     public String getMove(GameBoard b, Player p) {
         Graph virtualBoard = new Graph(b.COLUMNS * b.ROWS);
         Square[] path = virtualBoard.buildPath(b,p);
+        Square[] pathShort = path;
         
         int pno = p.getPlayerNo();
-        
-        Square[] pathShort = path;
         int winningPlayer = pno;
-        
-        for(int i = 0; i < b.getStartNumPlayers(); i++){
+       
+        // find which player has the shortest path 
+        for(int i = 0; i < b.numPlayersRemaining(); i++){
             Square [] temp = null;
-            if(i != pno)
-                temp = virtualBoard.buildPath(b,p);
-            if(temp != null && temp.length < (pathShort.length-1)){
+            if(i != pno && b.isPlayerRemaining(i))
+                temp = virtualBoard.buildPath(b,b.getPlayer(i));
+            if(temp != null && temp.length < (pathShort.length)){
                 pathShort = temp;
                 winningPlayer = i;
             }
         }
-        
+
+        // if this AI is not the Player with the shortest path,
+        //  try to block the opponent with the shortest path
         if(winningPlayer != pno){
-            String block = blockPlayer(b, winningPlayer, pathShort);
-            if(block != null)
+            // get a wall-blocking string
+            String block = blockPlayer(b, winningPlayer,pno, pathShort);
+            // if we have a valid wall-blocking string, return it
+            if(block != null) {
                 return block;
+            }
         }
         
+        // otherwise, return the next move in the shortest path
         return path[0].toString();
     }
 
-    public String blockPlayer(GameBoard b, int pno, Square[] path){
-        Square[] wall = new Square [2];
-        wall[0] = path [0];
-        if(pno == 0 || pno == 1){
-            int x = path[0].getX();
-            x++;
-            if(x > 8)
-                x-=2;
-            int y = path[0].getY();
-            if(pno == 1)
-                y--;
-            if(y < 0)
-                y+=2;
-            wall[1] = b.getSquare(x, y);
-        } else {
-            int y = path[0].getY();
-            y++;
-            if(y > 8)
-                y-=2;
-            int x = path[0].getX();
-            if(pno == 3)
-                x--;
-            if(x < 0)
-                x+=2;
-            wall[1] = b.getSquare( x, y);
+
+    /**
+      * Attempts to block an opposing player's path.
+      *     @param b GameBoard to retrieve locations from
+      *     @param opponentNo the ID number of the Player we wish to block
+      *     @param us the AI's Player number
+      *     @param path the path of the opposing player
+      */
+    public String blockPlayer(GameBoard b, int opponentNo,int us, Square[] path){
+        Square[] wallSquares = new Square [2];
+        
+        Random rand = new Random(System.currentTimeMillis());
+        int r = (rand.nextInt(path.length) - 1) % 1;
+        r = -1;
+    
+        int blockTwoX, blockTwoY;
+        Square blockOne;
+
+        // if random is negative, block around opponent's square
+        if ( r == -1 ) {
+            blockTwoX = path[0].getX();
+            blockTwoY = path[0].getY();
+            wallSquares[0] = path[0];
+            blockOne = b.getPlayerLoc(opponentNo);
+        }
+        // else, block further along opponent's path
+        else {
+            blockTwoX = path[r+1].getX();
+            blockTwoY = path[r+1 ].getY();
+            wallSquares[0] = path[r];
+            blockOne = path[r];
         }
 
-        if(GameEngine.validateWall(b, wall))
-            return ("(" + path[0].toString() + "," + path[1].toString() + ")");
+        // if the opponent wants to move verticallblockTwoY
+        if ( blockTwoX == blockOne.getX() ) {
+            blockTwoX++;
+            // if the opponent wants to move DOWN
+            if ( blockTwoY > blockOne.getY() ) {
+                wallSquares[0] = blockOne;
+                blockTwoY--;
+            }
+        }
+        // if the opponent wants to move horizontallblockTwoY
+        else {
+            blockTwoY++;
+            // if the opponent wants to move right
+            if ( blockTwoX > blockOne.getX() ) {
+                wallSquares[0] = blockOne;
+                blockTwoX--;
+            }
+        }
 
-        return null;
+        // assign second wall piece
+        wallSquares[1] = b.getSquare(blockTwoX,blockTwoY);
+
+        // validate the bounds of the second wall piece
+        if(wallSquares[1] == null) {
+            // if blockTwoX exceeds the right boundary
+            if ( blockTwoX >= b.COLUMNS ) {
+                wallSquares[1] = wallSquares[0];
+                wallSquares[0] = b.getSquare(blockTwoX-2,blockTwoY);
+            }
+            // if blockTwoX exceeds the left boundary
+            else if ( blockTwoX < 0 ) { 
+                wallSquares[1] = b.getSquare(blockTwoX+2,blockTwoY);
+            }
+            // if blockTwoY exceeds the bottom boundary
+            else if ( blockTwoY >= b.ROWS ) {
+                wallSquares[1] = wallSquares[0];
+                wallSquares[0] = b.getSquare(blockTwoX,blockTwoY-2);          
+            }
+            // if blockTwoY exceeds the top boundary
+            else {
+                wallSquares[1] = b.getSquare(blockTwoX,blockTwoY+2);
+            }
+        } 
+
+        String wallString = "(" + wallSquares[0].toString() + "," + wallSquares[1].toString() + ")";
+
+        // return the wall string if it is a valid wall placement on the GameBoard
+        return (GameEngine.validate(b,b.getPlayer(us), wallString) != null) ? wallString : null;
     }
 
     /**
