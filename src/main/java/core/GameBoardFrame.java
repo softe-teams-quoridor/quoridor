030104b3aeefc041cf07bd37ef53cf709bcc154e
Collin <collin.walling@gmail.com>
2015-05-11 13:43:01 -0400

    Frame: removed import java.net.URL

--- src/main/java/core/GameBoardFrame.java
+++ src/main/java/core/GameBoardFrame.java
@@ -22,15 +22,12 @@
  * findIcons()					--> searches directorys for file. Depreciated now.
  */
 
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-import java.net.URL;
 
 //Handles the construction of the GUI
 public class GameBoardFrame extends JFrame{
